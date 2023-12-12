1cf6c08a0819d641a83f39ec044aeb9e65adeb78
Paul Seyfert <pseyfert.mathphys@gmail.com>
2023-12-12 08:56:57 +0100

    day 12 part 2

--- 11/rust/src/main.rs
+++ 11/rust/src/main.rs
@@ -4,7 +4,40 @@ use just_a_filename::prelude::*;
 use rayon::prelude::*;
 use thiserror::Error;
 
-use std::{io::BufRead, str::FromStr};
+use std::{collections::HashSet, io::BufRead, str::FromStr};
+
+fn galaxy_distances(
+    galaxies: &HashSet<(usize, usize)>,
+    empty_rows: &[usize],
+    empty_columns: &[usize],
+    expansion: i64
+) -> i64 {
+    galaxies
+        .iter()
+        .combinations(2)
+        .map(|comb| {
+            let mut it = comb.iter();
+            let (Some(a), Some(b), None) = (it.next(), it.next(), it.next()) else {
+                panic!("combinations are strange");
+            };
+            // TODO: std::cmp::minmax
+            let (high, low) = (std::cmp::min(a.0, b.0), std::cmp::max(a.0, b.0));
+            let (left, right) = (std::cmp::min(a.1, b.1), std::cmp::max(a.1, b.1));
+            (high..low)
+                .map(|r| match empty_rows.binary_search(&r) {
+                    Ok(_) => expansion,
+                    Err(_) => 1,
+                })
+                .sum::<i64>()
+                + (left..right)
+                    .map(|c| match empty_columns.binary_search(&c) {
+                        Ok(_) => expansion,
+                        Err(_) => 1,
+                    })
+                    .sum::<i64>()
+        })
+        .sum::<i64>()
+}
 
 fn main() {
     let input_file = just_a_filename::Cli::parse().path;
@@ -40,6 +73,10 @@ fn main() {
     println!("empty columns: {empty_columns:?}");
     println!("empty rows: {empty_rows:?}");
 
-    let quick_test = galaxies.iter().combinations(2).count();
-    println!("need to do {quick_test} pairs");
+    let p1 = galaxy_distances(&galaxies, &empty_rows, &empty_columns, 2);
+    let p2_ex = galaxy_distances(&galaxies, &empty_rows, &empty_columns, 10);
+    let p2 = galaxy_distances(&galaxies, &empty_rows, &empty_columns, 1000000);
+    println!("{p1}");
+    println!("{p2_ex}");
+    println!("{p2}");
 }
